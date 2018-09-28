2ad4914dbea74a527cf4f2c3adae98c10d8b5c95
QuestionPro <qp@QuestionPros-MacBook-Pro.local>
2018-09-28 15:22:10 +0530

    Login Data

--- Hackhathon/Hackhathon/ViewController.swift
+++ Hackhathon/Hackhathon/ViewController.swift
@@ -9,11 +9,16 @@
 import UIKit
 import Firebase
 import FirebaseAuth
+import FirebaseDatabase
 
 class ViewController: UIViewController {
 
+    @IBOutlet var usernameTxt: UITextField!
+    @IBOutlet var passwordTxt: UITextField!
+    @IBOutlet var submitButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        loginLogicForEmailAndPassword(email: "tanvigpfse3@gmail.com")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -22,9 +27,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func loginLogicForEmailAndPassword(email: String, password: String) {
- 
+    func loginLogicForEmailAndPassword(email: String) {
+        Auth.auth().createUser(withEmail: email, password: "rt7qhqjgw") { (result, error) in
+            if let error = error {
+                if let errCode = AuthErrorCode(rawValue: error._code) {
+                    switch errCode {
+                    case .invalidEmail:
+                        print("Enter a valid email.")
+                    case .emailAlreadyInUse:
+                        print("Email already in use.")
+                    default:
+                        print("Error: \(error.localizedDescription)")
+                    }
+                }
+        }
+            if let _ = Auth.auth().currentUser {
+               
+            }
+        
+        }
     }
-
+    //MARK: Submit Button Clicked
+    @IBAction func submitButtonClicked() {
+        loginLogicForEmailAndPassword(email: usernameTxt.text!)
+    }
 }
 
